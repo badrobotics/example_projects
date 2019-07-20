047fc3a44b57217cdfd04ba2f0d45e2d1c13e550
Alexander Wranovsky <alex@wranovsky.org>
2019-07-20 15:53:35 -0500

    I think I finally have these peripheral singletons figured out.

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,39 @@ mod system_control;
 mod gpio;
 mod uart;
 
-use crate::tm4c1294_peripherals::SYSTEM_CONTROL;
-use crate::tm4c1294_peripherals::GPION;
+use crate::tm4c1294_peripherals::get_peripherals;
 
 const XTAL_FREQ: u32 = 25_000_000;
 const CPU_FREQ: u32 = 120_000_000;
 
 #[no_mangle]
 pub fn main() -> ! {
-    let _cpu_freq = unsafe { SYSTEM_CONTROL.tm4c129_config_sysclk(CPU_FREQ, XTAL_FREQ) };
-    unsafe { SYSTEM_CONTROL.enable_gpio_clock(system_control::GpioPort::GpioN) };
-    unsafe { GPION.configure_as_output(gpio::Pin::Pin0) };
-    loop {
-        unsafe {
-            GPION.set_low(gpio::Pin::Pin0);
-            let mut i = 50_000_000;
-            while i > 0 {
-                i = i - 1;
-            }
+    let mut p = get_peripherals();
+    let sysctl = p.take_system_control().unwrap();
+    let gpion = p.take_gpion().unwrap();
 
-            GPION.set_high(gpio::Pin::Pin0);
-            let mut i = 50_000_000;
-            while i > 0 {
-                i = i - 1;
-            }
+    let _cpu_freq = sysctl.tm4c129_config_sysclk(CPU_FREQ, XTAL_FREQ);
+
+    sysctl.enable_gpio_clock(system_control::GpioPort::GpioN);
+    gpion.configure_as_output(gpio::Pin::Pin0);
+    gpion.configure_as_output(gpio::Pin::Pin1);
+    gpion.configure_as_output(gpio::Pin::Pin2);
+
+    loop {
+        gpion.set_low(gpio::Pin::Pin0);
+        gpion.set_low(gpio::Pin::Pin1);
+        gpion.set_low(gpio::Pin::Pin2);
+        let mut i = 5_000_000;
+        while i > 0 {
+            i = i - 1;
+        }
+
+        gpion.set_high(gpio::Pin::Pin0);
+        gpion.set_high(gpio::Pin::Pin1);
+        gpion.set_high(gpio::Pin::Pin2);
+        let mut i = 5_000_000;
+        while i > 0 {
+            i = i - 1;
         }
     }
 }
--- src/tm4c1294_peripherals.rs
+++ src/tm4c1294_peripherals.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use core::mem::replace;
+
 use crate::system_control::SystemControl;
 use crate::gpio::Gpio;
 use crate::uart::Uart;
@@ -10,4 +12,40 @@ extern "C" {
     pub static mut GPIOA: Gpio;
     pub static mut GPION: Gpio;
     pub static mut UART0: Uart;
+}
+
+pub struct TM4C129Peripherals {
+    system_control: Option<&'static mut SystemControl>,
+    gpioa:          Option<&'static mut Gpio>,
+    gpion:          Option<&'static mut Gpio>,
+    uart0:          Option<&'static mut Uart>,
+}
+
+impl TM4C129Peripherals {
+    pub fn take_system_control(&mut self) -> Option<&'static mut SystemControl> {
+        replace(&mut self.system_control, None)
+    }
+
+    pub fn take_gpion(&mut self) -> Option<&'static mut Gpio> {
+        replace(&mut self.gpion, None)
+    }
+
+    pub fn take_gpioa(&mut self) -> Option<&'static mut Gpio> {
+        replace(&mut self.gpioa, None)
+    }
+
+    pub fn take_uart0(&mut self) -> Option<&'static mut Uart> {
+        replace(&mut self.uart0, None)
+    }
+}
+
+pub fn get_peripherals() -> TM4C129Peripherals {
+    unsafe {
+        TM4C129Peripherals {
+            system_control: Some(&mut SYSTEM_CONTROL),
+            gpioa: Some(&mut GPIOA),
+            gpion: Some(&mut GPION),
+            uart0: Some(&mut UART0),
+        }
+    }
 }
