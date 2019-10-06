2c0844a1aab9305cb291d366921cf3734f538967
Alexander Wranovsky <alex@wranovsky.org>
2019-10-06 14:53:14 -0500

    Got timers working

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,77 @@
 #![no_std]
 #![no_main]
 
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-extern crate tm4c129x_hal as hal;
 extern crate atomic_queue;
 
 use core::fmt::Write;
-use hal::prelude::*;
+
+use rust_tm4c::cortex_m4_init::enable_interrupts;
+use rust_tm4c::tm4c_peripherals::get_peripherals;
+use rust_tm4c::gpio;
+use rust_tm4c::system_control;
+use rust_tm4c::uart;
+use rust_tm4c::timer;
+use rust_tm4c::interrupt;
+
 use atomic_queue::AtomicQueue;
+
+const CPU_FREQ: u32  = 120_000_000;
+const XTAL_FREQ: u32 = 25_000_000;
 
 #[no_mangle]
 pub fn main() -> ! {
-    let p = hal::Peripherals::take().unwrap();
+    let mut p = get_peripherals();
+    let scb = p.take_scb().unwrap();
+    let nvic = p.take_nvic().unwrap();
+    let sysctl = p.take_system_control().unwrap();
+    let gpion = p.take_gpion().unwrap();
+    let gpioa = p.take_gpioa().unwrap();
+    let mut uart0 = p.take_uart0().unwrap();
+    let timer0 = p.take_timer0().unwrap();
 
-    let mut sc = p.SYSCTL.constrain();
-    sc.clock_setup.oscillator = hal::sysctl::Oscillator::Main(
-        hal::sysctl::CrystalFrequency::_16mhz,
-        hal::sysctl::SystemClock::UsePll(hal::sysctl::PllOutputFrequency::_120mhz),
-    );
-    let clocks = sc.clock_setup.freeze();
+    // Configure the CPU for the maximum operating frequency
+    let cpu_freq = sysctl.tm4c129_config_sysclk(CPU_FREQ, XTAL_FREQ);
 
-    let mut porta = p.GPIO_PORTA_AHB.split(&sc.power_control);
+    // Enable interrupts
+    unsafe { enable_interrupts(); }
 
-    // Activate UART
-    let mut uart = hal::serial::Serial::uart0(
-        p.UART0,
-        porta
-            .pa1
-            .into_af_push_pull::<hal::gpio::AF1>(&mut porta.control),
-        porta
-            .pa0
-            .into_af_push_pull::<hal::gpio::AF1>(&mut porta.control),
-        (),
-        (),
-        115200_u32.bps(),
-        hal::serial::NewlineMode::SwapLFtoCRLF,
-        &clocks,
-        &sc.power_control,
-    );
+    // Set up LEDs
+    sysctl.enable_gpio_clock(system_control::GpioPort::GpioN);
+    gpion.configure_as_output(gpio::Pin::Pin0);
+    gpion.configure_as_output(gpio::Pin::Pin1);
+    unsafe { GPIO_BLOCK = Some(gpion); }
+
+    // Set up the debug UART
+    sysctl.enable_gpio_clock(system_control::GpioPort::GpioA);
+    sysctl.enable_uart_clock(system_control::Uart::Uart0);
+    gpioa.select_alternate_function(gpio::Pin::Pin0, 1);
+    gpioa.select_alternate_function(gpio::Pin::Pin1, 1);
+    let _baud = uart0
+        .configure(
+            CPU_FREQ,
+            115200,
+            uart::Parity::None,
+            uart::StopBits::One,
+            uart::WordLength::Eight,
+        )
+        .unwrap();
+    let mut uart_driver = uart::drivers::UartBlockingDriver::new(&mut uart0, uart::drivers::NewlineMode::CRLF);
+
+    // Set up the timer
+    sysctl.enable_timer_clock(system_control::Timer::Timer0);
+    scb.int_register(interrupt::IntType::Timer0A, timer0a_handler);
+    match timer0.set_periodic_mode_32bit(cpu_freq / 2) {
+        Err(_) => panic!("Can't call this func?"),
+        _ => {},
+    };
+    match timer0.enable_timeout_interrupt_32bit() {
+        Err(_) => panic!("Can't call this func?"),
+        _ => {},
+    };
+    unsafe { TIMER_BLOCK = Some(timer0); }
+    nvic.clear_pending(interrupt::IntType::Timer0A);
+    nvic.set_priority(interrupt::IntType::Timer0A, 0);
+    nvic.enable(interrupt::IntType::Timer0A);
 
     // Create the queue
     let mut storage: [u8; 2] = [0; 2];
@@ -56,13 +90,32 @@ pub fn main() -> ! {
         Ok(_) => {},
     }
 
-    let mut counter = 0u8;
+    let mut counter = 0_u8;
     loop {
-        writeln!(uart, "Hello, world! counter={} two_values_ago={}", counter, queue.pop().unwrap()).unwrap();
+        //nvic.set_pending(interrupt::IntType::Timer0A);
+        writeln!(uart_driver, "Hello, world! counter={} two_values_ago={}", counter, queue.pop().unwrap()).unwrap();
         match queue.push(counter) {
             Err(_) => panic!("No room to push?"),
             Ok(_) => {},
         }
         counter = counter.wrapping_add(1);
+    }
+}
+
+static mut GPIO_BLOCK: Option<&'static mut gpio::Gpio> = None;
+static mut TIMER_BLOCK: Option<&'static mut timer::Timer> = None;
+pub unsafe extern "C" fn timer0a_handler() {
+    if let Some(t) = &mut TIMER_BLOCK {
+        match t.clear_timeout_interrupt_32bit() {
+            _ => {},
+        }
+    }
+
+    if let Some(g) = &mut GPIO_BLOCK {
+        if g.get(gpio::Pin::Pin0) == 0 {
+            g.set_high(gpio::Pin::Pin0);
+        } else {
+            g.set_low(gpio::Pin::Pin0);
+        }
     }
 }
