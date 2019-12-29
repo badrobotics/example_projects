250362437105ca10c2f6a553e518a64f2e5e963b
dkitzman <dakota.kitzman@gmail.com>
2019-12-28 20:09:15 -0600

    working test of a couple basic tasks

--- fertos_project/openocd.gdb
+++ fertos_project/openocd.gdb
@@ -4,14 +4,14 @@ target extended-remote :3333
 set print asm-demangle on
 
 # detect unhandled exceptions, hard faults and panics
-break DefaultHandler
+break DefaultExceptionHandler
 break UserHardFault
 break rust_begin_unwind
 
 # *try* to stop at the user entry point (it might be gone due to inlining)
 break main
 
-monitor arm semihosting enable
+# monitor arm semihosting enable
 
 # # send captured ITM to the file itm.fifo
 # # (the microcontroller SWO pin must be connected to the programmer SWO pin)
--- fertos_project/src/main.rs
+++ fertos_project/src/main.rs
@@ -1,32 +1,55 @@
 #![no_std]
 #![no_main]
 
-extern crate panic_halt;
-extern crate tm4c129x_hal as hal;
 extern crate alloc;
-extern crate fe_rtos;
 extern crate fe_osi;
-extern crate cortex_m_rt as rt; // v0.5.x
+use fe_rtos;
+use rust_tm4c;
+use tm4c123x_hal as hal;
 
+use alloc::boxed::Box;
 use core::fmt::Write;
-use cortex_m_rt::entry;
+use cortex_m::peripheral::scb::Exception;
 use hal::prelude::*;
-use alloc::vec;
 
-#[entry]
+const CPU_FREQ: u32 = 80_000_000;
+static mut COUNTER: u8 = 0;
+
+fn counter_task(_: &mut u32) {
+    unsafe {
+        loop {
+            COUNTER = COUNTER.wrapping_add(1);
+            fe_osi::sleep(1000);
+        }
+    }
+}
+
+fn hello_world<T: Write>(writer: &mut T) {
+    unsafe {
+        loop {
+            writeln!(writer, "Hello, world! seconds={}", COUNTER).unwrap();
+            fe_osi::sleep(100);
+        }
+    }
+}
+
+#[no_mangle]
 fn main() -> ! {
     let p = hal::Peripherals::take().unwrap();
+    let cp = hal::CorePeripherals::take().unwrap();
+
     let mut sc = p.SYSCTL.constrain();
+    let systick = cp.SYST;
     sc.clock_setup.oscillator = hal::sysctl::Oscillator::Main(
-        hal::sysctl::CrystalFrequency::_25mhz,
-        hal::sysctl::SystemClock::UsePll(hal::sysctl::PllOutputFrequency::_120mhz),
+        hal::sysctl::CrystalFrequency::_16mhz,
+        hal::sysctl::SystemClock::UsePll(hal::sysctl::PllOutputFrequency::_80_00mhz),
     );
     let clocks = sc.clock_setup.freeze();
 
-    let mut porta = p.GPIO_PORTA_AHB.split(&sc.power_control);
+    let mut porta = p.GPIO_PORTA.split(&sc.power_control);
 
     // Activate UART
-    let mut uart = hal::serial::Serial::uart0(
+    let uart0 = hal::serial::Serial::uart0(
         p.UART0,
         porta
             .pa1
@@ -42,11 +65,32 @@ fn main() -> ! {
         &sc.power_control,
     );
 
-    let _test_vec = vec![0,1,2,3];
+    rust_tm4c::interrupt::int_register(
+        (Exception::SysTick.irqn() + 16) as u32,
+        fe_rtos::task::sys_tick,
+    );
+    rust_tm4c::interrupt::int_register(
+        (Exception::PendSV.irqn() + 16) as u32,
+        fe_rtos::task::context_switch,
+    );
+    rust_tm4c::interrupt::int_register(
+        (Exception::SVCall.irqn() + 16) as u32,
+        fe_rtos::syscall::svc_handler,
+    );
 
-    let mut counter = 0u32;
-    loop {
-        writeln!(uart, "Hello, world! counter={}", counter).unwrap();
-        counter = counter.wrapping_add(1);
+    unsafe {
+        fe_rtos::task::add_task(
+            fe_rtos::task::DEFAULT_STACK_SIZE,
+            hello_world,
+            Some(Box::new(uart0)),
+        );
+        fe_rtos::task::add_task(fe_rtos::task::DEFAULT_STACK_SIZE, counter_task, None);
     }
+
+    let reload_val: u32 = CPU_FREQ / 10000;
+
+    // Start the FeRTOS scheduler
+    fe_rtos::task::start_scheduler(cortex_m::peripheral::SCB::set_pendsv, systick, reload_val);
+
+    loop {}
 }
