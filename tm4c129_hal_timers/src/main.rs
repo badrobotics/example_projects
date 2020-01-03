b2d8a0a3d263d9ef6e58f2ea889445d7e0decbcc
Alexander Wranovsky <alex@wranovsky.org>
2020-01-02 22:37:07 -0600

    Refactored the timer initialization code into a macro so that it's easy to instantiate it multipe times. Unfortunately I can't get it to compile in its current state, so I may have to use a different approach.

--- tm4c129_hal_timers/src/main.rs
+++ tm4c129_hal_timers/src/main.rs
@@ -7,8 +7,9 @@ extern crate cortex_m_semihosting as sh;
 extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 //extern crate tm4c123x_hal as hal;
 extern crate tm4c129x_hal as hal;
-extern crate tm4c129x;
+extern crate tm4c129x as device;
 extern crate atomic_queue;
+extern crate embedded_hal;
 #[macro_use]
 extern crate lazy_static;
 
@@ -19,14 +20,45 @@ use rt::exception;
 use rt::entry;
 use sh::hio;
 
-use tm4c129x::interrupt;
+use device::interrupt;
 
 use hal::prelude::*;
-use hal::timer::TIMER0;
+use hal::timer::{TIMER0, TIMER1, TIMER2, TIMER3, TIMER4, TIMER5};
 use hal::time::Hertz;
 use hal::timer::Event;
 
 use atomic_queue::AtomicQueue;
+
+macro_rules! timer_interrupt_macro {
+    ( $($int_name:ident,$timer_path:ident,$timer_num:tt;)+ ) => {
+        $(
+            #[interrupt]
+            fn $int_name() {
+                // clear the interrupt
+                unsafe { (*device::$timer_path::ptr()).icr.write(|w| w.tatocint().bit(true)); }
+                match MSG_QUEUE.push($timer_num) {
+                    _ => {},
+                }
+            }
+        )+
+    }
+}
+
+macro_rules! configure_timers {
+    ( $pc_ref:expr, $clk_ref:expr ; $( ($peripheral:expr, $hal_func:path, $interrupt:ident) ,)+ ) => {
+        [ $( {
+            let ref mut tim = $hal_func (
+                $peripheral,
+                Hertz(10),
+                $pc_ref,
+                $clk_ref,
+            );
+            unsafe { device::NVIC::unmask(device::Interrupt::$interrupt); }
+            tim.listen(Event::TimeOut);
+            tim as &mut dyn embedded_hal::timer::CountDown
+        }, )+ ]
+    }
+}
 
 static mut MSG_QUEUE_STORAGE: [u8; 100] = [0; 100];
 lazy_static! {
@@ -53,11 +85,6 @@ fn main() -> ! {
     );
     let clocks = sc.clock_setup.freeze();
 
-    writeln!(stdout, "Setting up interrupts").unwrap();
-    unsafe {
-        tm4c129x::NVIC::unmask(tm4c129x::Interrupt::TIMER0A);
-    }
-
     writeln!(stdout, "Enabling GPIO PORTA").unwrap();
     let mut porta = peripherals.GPIO_PORTA_AHB.split(&sc.power_control);
 
@@ -78,23 +105,25 @@ fn main() -> ! {
         &sc.power_control,
     );
 
-    // Activate Timer
-    let mut timer0 = hal::timer::Timer::<TIMER0>::timer0(
-        peripherals.TIMER0,
-        Hertz(2),
-        &sc.power_control,
-        &clocks,
-    );
-    timer0.listen(Event::TimeOut);
+    let mut timers: [&mut dyn embedded_hal::timer::CountDown<Time=Hertz>] = configure_timers! [ &sc.power_control, &clocks;
+        (peripherals.TIMER0, hal::timer::Timer::<TIMER0>::timer0, TIMER0A),
+        (peripherals.TIMER1, hal::timer::Timer::<TIMER1>::timer1, TIMER1A),
+        (peripherals.TIMER2, hal::timer::Timer::<TIMER2>::timer2, TIMER2A),
+        (peripherals.TIMER3, hal::timer::Timer::<TIMER3>::timer3, TIMER3A),
+        (peripherals.TIMER4, hal::timer::Timer::<TIMER4>::timer4, TIMER4A),
+        (peripherals.TIMER5, hal::timer::Timer::<TIMER5>::timer5, TIMER5A),
+    ];
 
     writeln!(stdout, "Starting timers. Listen on UART0 for timout messages.").unwrap();
-    timer0.start(Hertz(20));
+    for (timer, num) in timers.iter().enumerate() {
+        timer.start(Hertz(num));
+    }
 
     loop {
         match MSG_QUEUE.pop() {
-            Some(0) => {
-                writeln!(uart, "Timer 0").unwrap();
-                timer0.start(Hertz(20));
+            Some(num) => {
+                writeln!(uart, "Timer {}", num).unwrap();
+                timers[num].start(Hertz(num.into()));
             },
             _ => {},
         }
@@ -111,11 +140,11 @@ fn DefaultHandler(irqn: i16) {
     panic!("Unhandled exception (IRQn = {})", irqn);
 }
 
-#[interrupt]
-fn TIMER0A() {
-    // clear the interrupt
-    unsafe { (*tm4c129x::TIMER0::ptr()).icr.write(|w| w.tatocint().bit(true)); }
-    match MSG_QUEUE.push(0) {
-        _ => {},
-    }
+timer_interrupt_macro! {
+    TIMER0A, TIMER0, 0;
+    TIMER1A, TIMER1, 1;
+    TIMER2A, TIMER2, 2;
+    TIMER3A, TIMER3, 3;
+    TIMER4A, TIMER4, 4;
+    TIMER5A, TIMER5, 5;
 }
